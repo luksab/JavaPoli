ad9d9591142ecefb5912a69fe6175a85d5c8f036
luksab <luksab@users.noreply.github.com>
2015-07-21 11:25:44 +0200

    Create streets.js

---
+++ streets.js
@@ -0,0 +1,15 @@
+JavapoliImportedStreets = [
+		{name: "Start field", price: 4000, callback: "_streetFieldLanding", player: null, credit: 8000, rent: {plain: 10000, a1: 50000, a2: 100000, a3: 500000, a4: 1000000, h: 5000000}, status: "plain", group: null, group_factor: 2},
+		{name: "Donau Street", price: 4000, callback: "_streetFieldLanding", player: null, credit: 8000, rent: {plain: 10000, a1: 50000, a2: 100000, a3: 500000, a4: 1000000, h: 5000000}, status: "plain", group: 1, group_factor: 2},
+		{name: "Rhine Street", price: 4000, callback: "_streetFieldLanding", player: null, credit: 8.000, rent: {plain: 10000, a1: 50000, a2: 100000, a3: 500000, a4: 1000000, h: 5000000}, status: "plain", group: 1, group_factor: 2},
+		{name: "Urban Market", price: 4000, callback: "_streetFieldLanding", player: null, credit: 9000, rent: {plain: 10000, a1: 50000, a2: 100000, a3: 500000, a4: 1000000, h: 5000000}, status: "plain", group: 2, group_factor: 2},
+		{name: "Tree Alley", price: 4000, callback: "_streetFieldLanding", player: null, credi: 9000, rent: {plain: 10000, a1: 50000, a2: 100000, a3: 500000, a4: 1000000, h: 5000000}, status: "plain", group: 2, group_factor: 2},
+		{name: "Airport North", price: 4000, callback: "_streetFieldLanding", player: null, credit: 9000, rent: {plain: 10000, a1: 50000, a2: 100000, a3: 500000, a4: 1000000, h: 5000000}, status: "plain", group: 2, group_factor: 2},
+		{name: "Ford Plaza", price: 4000, callback: "_streetFieldLanding", player: null, credit: 8000, rent: {plain: 10000, a1: 50000, a2: 100000, a3: 500000, a4: 1000000, h: 5000000}, status: "plain", group: 3, group_factor: 2},
+		{name: "Eastern Pathway", price: 4000, callback: "_streetFieldLanding", player: null, credit: 8000, rent: {plain: 10000, a1: 50000, a2: 100000, a3: 500000, a4: 1000000, h: 5000000}, status: "plain", group: 3, group_factor: 2},
+		{name: "Millenium Drive", price: 4000, callback: "_streetFieldLanding", player: null, credit: 8000, rent: {plain: 10000, a1: 50000, a2: 100000, a3: 500000, a4: 1000000, h: 5000000}, status: "plain", group: 4, group_factor: 2},
+		{name: "Park Alley", price: 4000, callback: "_streetFieldLanding", player: null, credit: 8000, rent: {plain: 10000, a1: 50000, a2: 100000, a3: 500000, a4: 1000000, h: 5000000}, status: "plain", group: 4, group_factor: 2},
+		{name: "Seven Oaks", price: 4000, callback: "_streetFieldLanding", player: null, credit: 8000, rent: {plain: 10000, a1: 50000, a2: 100000, a3: 500000, a4: 1000000, h: 5000000}, status: "plain", group: 5, group_factor: 2},
+		{name: "Castle Road", price: 4000, callback: "_streetFieldLanding", player: null, credit: 8000, rent: {plain: 10000, a1: 50000, a2: 100000, a3: 500000, a4: 1000000, h: 5000000}, status: "plain", group: 5, group_factor: 2},
+		{name: "Mountain View", price: 4000, callback: "_streetFieldLanding", player: null, credit: 8000, rent: {plain: 10000, a1: 50000, a2: 100000, a3: 500000, a4: 1000000, h: 5000000}, status: "plain", group: 5, group_factor: 2}
+];
